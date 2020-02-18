d4dfb7971654c7273a383945bc8b462762466392
kand193 <qkrdudwls99@gmail.com>
2020-02-18 19:44:41 +0900

    feat: Initial setting for svelte

---
+++ README.md
@@ -0,0 +1,9 @@
+# PERSONAL PORTFOLIO SVELTE
+
+It's a simple project using svelte.
+
+## Tech Stack
+
+- Svelte
+- Javascript
+- etc.
---
+++ src/App.svelte
@@ -0,0 +1,9 @@
+<script>
+  const word = "WORLD";
+  const person = {
+    firstName: "YJ",
+    lastName: "P"
+  };
+</script>
+
+<h1>Hello {word} {person.firstName} {person.lastName}</h1>
